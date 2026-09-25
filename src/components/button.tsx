5b0a52fc67deeb9be2d394f35c549767f414b629
Abdou <abdousani60@gmail.com>
2026-09-25 18:12:45 +0200

    feat: amélioration de la page d'accueil avec nouveaux boutons, footer et design ajusté

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,16 +3,23 @@ import { cva } from 'class-variance-authority'
 import type { VariantProps } from 'class-variance-authority'
 
 const buttonVariants = cva(
-  'inline-flex cursor-pointer items-center justify-center gap-2 rounded-xl px-5 py-2 text-sm',
+  'inline-flex cursor-pointer items-center justify-center gap-2 px-3 py-2 text-sm',
   {
     variants: {
       variant: {
         primary: 'bg-button text-button-foreground',
-        secondary: 'bg-secondary text-secondary-foreground',
+        secondary: 'bg-[#94ECC9]',
+        transparent: 'bg-transparent',
+      },
+      radius: {
+        default: 'rounded-xl',
+        none: 'rounded-none',
+        curved: 'rounded-4xl',
       },
     },
     defaultVariants: {
       variant: 'primary',
+      radius: 'default',
     },
   },
 )
@@ -25,13 +32,14 @@ interface ButtonProps
 export default function Button({
   className,
   variant,
+  radius,
   type = 'button',
   ...props
 }: ButtonProps) {
   return (
     <button
       type={type}
-      className={buttonVariants({ variant, className })}
+      className={buttonVariants({ variant, radius, className })}
       {...props}
     />
   )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
 import { createFileRoute, Link } from '@tanstack/react-router'
 import { Logo } from '#/components/logo.tsx'
+import Button from '#/components/button.tsx'
+import { MoveRight } from 'lucide-react'
 
 export const Route = createFileRoute('/')({
   component: Home,
@@ -20,25 +22,52 @@ function Home() {
             <Logo />
           </div>
           <Link to="/dashboard">
-            <span className="text-muted-foreground">Dashboard</span>
+            <Button
+              variant="transparent"
+              radius="curved"
+              className="border-foreground border-2 font-bold"
+            >
+              Se connecter
+            </Button>
           </Link>
         </header>
         <main className="flex h-full w-full items-center justify-center gap-5 overflow-hidden">
-          <div className="flex h-full flex-1 flex-col justify-center text-5xl font-black">
-            <span>
+          <div className="flex h-full w-auto flex-1 flex-col justify-center gap-3 text-6xl font-black">
+            <p className="leading-18">
               Vos abonnements.
               <br />
-              <span>L'esprit léger.</span>
-            </span>
-            <span className="text-muted-foreground text-xl font-medium">
+              <mark className="w-auto bg-[#e3f2e9]">L'esprit léger.</mark>
+            </p>
+            <p className="text-muted-foreground text-xl font-medium">
               Gardez un oeil sur vos dépenses et vos échéances, <br />
               tout ça au même endroit.
-            </span>
+            </p>
+
+            <Button
+              variant="secondary"
+              radius="curved"
+              className="border-foreground w-1/2 border-2 font-bold"
+            >
+              Créer mon compte
+              <MoveRight className="text-foreground" />
+            </Button>
           </div>
-          <div className="flex h-full flex-1 text-2xl font-bold">
-            <img src="/assets/illustration.png" alt="illustration" />
+          <div className="flex flex-1 text-2xl font-bold">
+            <img
+              src="/assets/illustration.png"
+              alt="illustration"
+              className="max-h-full max-w-full object-contain"
+            />
           </div>
         </main>
+        <footer className="text-muted-foreground flex h-20 items-center justify-evenly">
+          <span>@Abdou-Rahmane Sani</span>
+          <div className="flex items-center gap-2">
+            <span>Confidentialité</span>
+            <span aria-hidden="true" className="h-4 w-px bg-border" />
+            <span>Contact</span>
+          </div>
+        </footer>
       </div>
     </>
   )
